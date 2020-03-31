dcb4c8a69587fbcfaa61ccba12a03f00ffed1ccd
Ricki Moore <mrricki.m.usmc@gmail.com>
2020-03-31 02:55:43 +0200

    count sockets online

--- server.js
+++ server.js
@@ -3,23 +3,19 @@ const Http = require('http').Server(Express);
 const SocketIo = require('socket.io')(Http);
 const PORT = process.env.PORT || 3000;
 
-
 SocketIo.on('connection', socket => {
-    let connectedCounter = 0;
-
     socket.on('onBoard', user => {
-        connectedCounter += 1;
         SocketIo.emit('introduce', user);
-        SocketIo.emit('count', connectedCounter);
+        var srvSockets = SocketIo.sockets.sockets;
+        SocketIo.emit('count', Object.keys(srvSockets).length);
     });
 
     socket.on('onLeave', user => {
-        connectedCounter -= 1;
-        if(connectedCounter < 0) {
-            connectedCounter = 0;
-        }
         SocketIo.emit('goodBye', user);
-        SocketIo.emit('count', connectedCounter);
+        setTimeout(() => {
+            var srvSockets = SocketIo.sockets.sockets;
+            SocketIo.emit('count', Object.keys(srvSockets).length)
+        }, 1000)
     });
 
     socket.on('message', message => {
