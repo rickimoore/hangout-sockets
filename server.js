302fac73ab6bdf367e31464097551f5de0b45e95
Ricki Moore <mrricki.m.usmc@gmail.com>
2020-04-05 12:07:15 +0200

    updated server for game lobby

--- server.js
+++ server.js
@@ -3,11 +3,47 @@ const Http = require('http').Server(Express);
 const SocketIo = require('socket.io')(Http);
 const PORT = process.env.PORT || 3000;
 
+let StartTime = null;
+let isOpenLobby = true;
+let checkLobbyInterval = null;
+let gameLobby = [];
+
+
+const closeLobby = function () {
+    console.log('starting game...')
+    clearInterval(checkLobbyInterval);
+    checkLobbyInterval = null;
+    isOpenLobby = false;
+    SocketIo.emit('START_GAME', gameLobby);
+    openLobby();
+};
+
+const openLobby = function () {
+    gameLobby = [];
+    isOpenLobby = true;
+};
+
+const startLobbyClose = function () {
+    console.log(isOpenLobby, checkLobbyInterval)
+    if(isOpenLobby && !checkLobbyInterval){
+        checkLobbyInterval = setInterval(() => {
+            console.log('checking lobby...');
+            if(gameLobby.length > 1){
+                closeLobby();
+            }
+        }, 1000);
+    }
+};
+
 SocketIo.on('connection', socket => {
     socket.on('onBoard', user => {
         SocketIo.emit('introduce', user);
         var srvSockets = SocketIo.sockets.sockets;
         SocketIo.emit('count', Object.keys(srvSockets).length);
+
+        if(StartTime){
+            SocketIo.emit('bufferMusic', StartTime);
+        }
     });
 
     socket.on('onLeave', user => {
@@ -20,6 +56,24 @@ SocketIo.on('connection', socket => {
 
     socket.on('message', message => {
         SocketIo.emit('relay', message)
+    });
+
+    socket.on('joinGameLobby', user => {
+       startLobbyClose();
+       gameLobby.push(user);
+    });
+
+    socket.on('joinTag', user => {
+        SocketIo.emit('joinPlayer', user)
+    });
+
+    socket.on('MOVE_USER', data => {
+        SocketIo.emit('MOVE_TOKEN', data)
+    });
+
+    socket.on('masterPlay', time => {
+        StartTime = time;
+        console.log('here')
     });
 });
 
